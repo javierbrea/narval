07c1836926462204a0c8252cf4bead8b449a83bc
Javier Brea <javier.brea@gmail.com>
2018-04-03 19:59:54 +0200

    Expose mocha-sinon-chai. Rename package

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-'use strict'
 
-const config = require('./lib/config')
-const options = require('./lib/options')
-const paths = require('./lib/paths')
-
-const configuration = config.read()
-
-if (options.standard) {
-  const standardPath = paths.findBin('standard')
-  console.log('Running standard')
-  require(standardPath)
-}
-
-if (options.unit && configuration.test && configuration.test.unit) {
-  console.log('Running unit tests')
-  require('mocha-sinon-chai/runner').run(['--', '--recursive', configuration.test.unit])
-}
+module.exports = require('mocha-sinon-chai')
