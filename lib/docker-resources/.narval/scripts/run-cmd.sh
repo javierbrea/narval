552be76e8cdf49731da0af9c59f51581f2187a15
Javier Brea <javier.brea@gmail.com>
2018-04-07 19:31:33 +0200

    Istanbul and mocha configuration available from test and coverage yaml config

--- lib/docker-resources/.narval/scripts/run-cmd.sh
+++ lib/docker-resources/.narval/scripts/run-cmd.sh
@@ -7,16 +7,17 @@ coverage_enabled=$4
 wait_for=$5
 exit_after=$6
 
-log_title="[Narval] <INFO>"
+log_title="[Narval] [DEBUG]"
+log_sep="================================"
 
-echo ""
-echo "$log_title RUN COMMAND OPTIONS:"
-echo "$log_title command_to_run: $command_to_run"
-echo "$log_title command_params: $command_params"
-echo "$log_title coverage_options: $coverage_options"
-echo "$log_title coverage_enabled: $coverage_enabled"
-echo "$log_title wait_for: $wait_for"
-echo "$log_title exit_after: $exit_after"
+# echo "$log_sep"
+# echo "$log_title RUN COMMAND OPTIONS:"
+# echo "$log_title command_to_run: $command_to_run"
+# echo "$log_title command_params: $command_params"
+# echo "$log_title coverage_options: $coverage_options"
+# echo "$log_title coverage_enabled: $coverage_enabled"
+# echo "$log_title wait_for: $wait_for"
+# echo "$log_title exit_after: $exit_after"
 
 if [ -z "$command_to_run" ]; then
   echo "Please define a command to be run"
@@ -41,9 +42,8 @@ else
     command_to_run=".narval/scripts/wait-for-it.sh $wait_for -- ./$command_to_run"
   fi
       
-  echo ""
+  echo "$log_sep"
   echo "$log_title RUNNING COMMAND: $command_to_run"
-  echo ""
 
   ./$command_to_run
 fi
