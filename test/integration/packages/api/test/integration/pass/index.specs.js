4df75bd06f9ebe8ce306cc3524cf8ee9e4e6573d
Javier Brea <javier.brea@gmail.com>
2018-05-05 20:37:10 +0200

    Add integration tests about local execution

--- test/integration/packages/api/test/integration/pass/index.specs.js
+++ test/integration/packages/api/test/integration/pass/index.specs.js
@@ -2,7 +2,12 @@
 const test = require('narval')
 
 test.describe('This tests always pass', function () {
-  test.it('should pass', () => {
-    test.expect(true).to.be.true()
+  this.timeout(10000)
+  test.it('should pass', (done) => {
+    // let time for services to error by itself before being foreced to exit because tests have finished OK
+    setTimeout(() => {
+      test.expect(true).to.be.true()
+      done()
+    }, 3000)
   })
 })
