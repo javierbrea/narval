63046f4ff8e9fec3ea1c6c12c1986c8005267c43
Javier Brea <javier.brea@gmail.com>
2018-05-06 09:21:17 +0200

    Finish integration tests about local executions

--- test/integration/specs/api-coverage/local.common.specs.js
+++ test/integration/specs/api-coverage/local.common.specs.js
@@ -14,6 +14,6 @@ test.describe('api-local-coverage suite execution', () => {
   })
 
   test.it('should have waited for api execution finish', () => {
-    return test.expect(outerrLog).to.include('[Narval] [DEBUG] Wait finished. "tcp:localhost:3000" is available')
+    return test.expect(outerrLog).to.include('[Narval] [DEBUG] Waiting until "tcp:localhost:3000" is available.')
   })
 })
