a9af0a02097340a0006da34f102ad0cb791be3b3
Javier Brea <javier.brea@gmail.com>
2018-05-06 11:32:42 +0200

    Add standard fix option integration test

---
+++ test/integration/specs/api-local-shell/ko.specs.js
@@ -0,0 +1,31 @@
+
+const test = require('../../../../index')
+const utils = require('../utils')
+
+test.describe('api-local tests execution', () => {
+  let outerrLog
+
+  test.before((done) => {
+    utils.readOutErr()
+      .then((log) => {
+        outerrLog = log
+        done()
+      })
+  })
+
+  test.it('should have waited for service to start tests', () => {
+    return test.expect(outerrLog).to.include('Waiting until "tcp:localhost:3000" is available.')
+  })
+
+  test.it('should have timed out waiting for service', () => {
+     return test.expect(outerrLog).to.include('Wait timed out. "tcp:localhost:3000" is not available.')
+  })
+
+  test.it('should have printed and error because the path to custom shell was not found', () => {
+    return test.expect(outerrLog).to.include('Error trying to run command. spawn /foo/path/to/shell ENOENT')
+  })
+
+  test.it('should have exited api-server with an error', () => {
+    return test.expect(outerrLog).to.match(/Service "api-server" closed with code [^0]/)
+  })
+})
