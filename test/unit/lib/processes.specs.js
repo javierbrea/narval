5e87031de1b8dd3c0fa1825efd291bb6a53f47f2
Javier Brea <javier.brea@gmail.com>
2018-05-16 14:57:37 +0200

    Developing processes unit tests

--- test/unit/lib/processes.specs.js
+++ test/unit/lib/processes.specs.js
@@ -1,5 +1,4 @@
 
-/* const Promise = require('bluebird')
 const fsExtra = require('fs-extra')
 
 const test = require('../../../index')
@@ -7,8 +6,7 @@ const mocks = require('../mocks')
 
 const processes = require('../../../lib/processes')
 
-test.describe.skip('processes', () => {
-  const fooPath = 'fooPath/fooFile.js'
+test.describe('processes', () => {
   let sandbox
   let mocksSandbox
   let childProcessMock
@@ -35,81 +33,119 @@ test.describe.skip('processes', () => {
     fsMock.restore()
   })
 
-  test.describe('fork method', () => {
-    test.it('should return a promise', () => {
-      return processes.fork()
-        .then(() => {
-          return test.expect(true).to.be.true()
-        })
-    })
+  const testChildProcessMethod = function (method) {
+    test.describe(`${method} method`, () => {
+      const fooPath = 'fooPath/fooFile.js'
 
-    test.it('should fork a child process with the received filePath', () => {
-      return processes.fork(fooPath)
-        .then(() => {
-          return test.expect(childProcessMock.stubs.fork.getCall(0).args[0]).to.equal(fooPath)
-        })
-    })
-
-    test.it('should pass the received arguments to the child process', () => {
-      const fooArgs = ['fooArg1', 'fooArg2']
-      return processes.fork(fooPath, {
-        args: fooArgs
-      }).then(() => {
-        return test.expect(childProcessMock.stubs.fork.getCall(0).args[1]).to.deep.equal(fooArgs)
+      test.it('should return a promise', () => {
+        return processes[method]()
+          .then(() => {
+            return test.expect(true).to.be.true()
+          })
       })
-    })
 
-    test.it('should pass an empty as arguments to the child process if no args is received in config', () => {
-      return processes.fork(fooPath)
-        .then(() => {
-          return test.expect(childProcessMock.stubs.fork.getCall(0).args[1]).to.deep.equal([])
+      test.it(`should ${method} a child process with the received filePath`, () => {
+        return processes[method](fooPath)
+          .then(() => {
+            return test.expect(childProcessMock.stubs[method].getCall(0).args[0]).to.equal(fooPath)
+          })
+      })
+
+      test.it('should pass the received arguments to the child process', () => {
+        const fooArgs = ['fooArg1', 'fooArg2']
+        return processes[method](fooPath, {
+          args: fooArgs
+        }).then(() => {
+          return test.expect(childProcessMock.stubs[method].getCall(0).args[1]).to.deep.equal(fooArgs)
         })
-    })
+      })
 
-    test.it('should pass the received options to the chid process, adding the cwd path', () => {
-      const fooCwdPath = 'fooCwdPath/testing'
-      const fooOptions = {
-        env: {
-          fooEnv: 'testing'
-        }
-      }
-      pathsMock.stubs.cwd.base.returns(fooCwdPath)
-      return processes.fork(fooPath, {
-        options: fooOptions
-      }).then(() => {
-        return test.expect(childProcessMock.stubs.fork.getCall(0).args[2]).to.deep.equal({
-          cwd: fooCwdPath,
+      test.it('should pass an empty as arguments to the child process if no args is received in config', () => {
+        return processes[method](fooPath)
+          .then(() => {
+            return test.expect(childProcessMock.stubs[method].getCall(0).args[1]).to.deep.equal([])
+          })
+      })
+
+      test.it('should pass the received options to the child process, adding the cwd path', () => {
+        const fooCwdPath = 'fooCwdPath/testing'
+        const fooOptions = {
           env: {
-            fooEnv: fooOptions.env.fooEnv
+            fooEnv: 'testing'
           }
+        }
+        mocksSandbox.paths.stubs.cwd.base.returns(fooCwdPath)
+        return processes[method](fooPath, {
+          options: fooOptions
+        }).then(() => {
+          return test.expect(childProcessMock.stubs[method].getCall(0).args[2]).to.deep.equal({
+            cwd: fooCwdPath,
+            env: {
+              fooEnv: fooOptions.env.fooEnv
+            }
+          })
+        })
+      })
+
+      test.describe('when it is configured to resolve the promise on process close', () => {
+        test.it('should resolve the promise with the received exit code', () => {
+          const fooExitCode = 345
+          childProcessMock.stubs[method].on.returns(fooExitCode)
+          return processes[method](fooPath, {
+            resolveOnClose: true
+          })
+            .then((code) => {
+              return test.expect(code).to.equal(fooExitCode)
+            })
+        })
+      })
+
+      test.describe('when it is not configured to resolve the promise on process close', () => {
+        test.it('should resolve the promise with the opened child process', () => {
+          return processes[method](fooPath)
+            .then((childProcess) => {
+              return Promise.all([
+                test.expect(typeof childProcess.on).to.equal('function'),
+                method === 'fork' ? test.expect(typeof childProcess.send).to.equal('function') : test.expect(childProcess.send).to.be.undefined()
+              ])
+            })
         })
       })
     })
+  }
 
-    test.describe('when it is configured to resolve the promise on process close', () => {
-      test.it('should resolve the promise with the received exit code', () => {
-        const fooExitCode = 345
-        childProcessMock.stubs.fork.on.returns(fooExitCode)
-        return processes.fork(fooPath, {
-          resolveOnClose: true
-        })
-          .then((code) => {
-            return test.expect(code).to.equal(fooExitCode)
-          })
-      })
+  testChildProcessMethod('fork')
+  testChildProcessMethod('spawn')
+
+  test.describe('execSync method', () => {
+    test.it('should open an execSync child process with the received command and options', () => {
+      const fooCommand = 'foo sync command'
+      const fooOptions = {
+        fooOption1: 'foo val',
+        fooOption2: true
+      }
+      processes.execSync(fooCommand, fooOptions)
+      test.expect(childProcessMock.stubs.execSync.getCall(0).args[0]).to.equal(fooCommand)
+      test.expect(childProcessMock.stubs.execSync.getCall(0).args[1]).to.equal(fooOptions)
+    })
+  })
+
+  test.describe('Handler constructor', () => {
+    const fooSuiteData = {
+      type: 'fooType',
+      suite: 'fooSuite',
+      service: 'fooService'
+    }
+    let fooProcess
+    test.beforeEach(() => {
+      fooProcess = childProcessMock.stubs.fork()
     })
 
-    test.describe('when it is not configured to resolve the promise on process close', () => {
-      test.it('should resolve the promise with the opened child process', () => {
-        return processes.fork(fooPath)
-          .then((childProcess) => {
-            return Promise.all([
-              test.expect(typeof childProcess.on).to.equal('function'),
-              test.expect(typeof childProcess.send).to.equal('function')
-            ])
-          })
-      })
-    }) */
+    test.it.skip('should return an event bus', () => {
+      test.expect(new processes.Handler(fooProcess, fooSuiteData).on).to.not.be.undefined()
+    })
+  })
+})
 
 /* test.describe('when logging data from the child process', () => {
       const option = {
@@ -144,6 +180,4 @@ test.describe.skip('processes', () => {
           return test.expect(console.log).to.not.have.been.calledWith(fooData)
         })
       })
-    })
-  })
-}) */
+    }) */
