1d13ace1305574229cd7897b94b4f94d027e5c09
Javier Brea <javier.brea@gmail.com>
2018-05-25 20:22:52 +0200

    Ignore .narval folders inside foo testing packages

--- test/integration/packages/api/.narval/docker/mongodb-image/docker-resources/.narval/scripts/run-and-exit-after.js
+++
@@ -1,7 +0,0 @@
-
-setTimeout(() => {
-  console.log(`Service timeout finished after ${process.env.exit_after}ms. Exiting...`)
-  process.exit(0)
-}, process.env.exit_after)
-
-require('../../' + process.env.command_to_coverage)
